73dc938c976151e75fa579e4ba6adbf8e65037f9
Zhanyl <zh.kishkentai@gmail.com>
2025-02-26 10:09:06 +0500

    Выполнение задачи недели 5 уровня hard

--- Module 5/Homework/src/five/hard/Main.java
+++ Module 5/Homework/src/five/hard/Main.java
@@ -1,14 +1,36 @@
 package five.hard;
 
 import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество чисел: ");
+        int n = scanner.nextInt();
 
-  }
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("Введите " + n + " чисел:");
+        for (int i = 0; i < n; i++) {
+            numbers.add(scanner.nextInt());
+        }
+        scanner.close();
 
-  public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
-    return null;
-  }
+        ArrayList<Integer> uniqueNumbers = removeDuplicates(numbers);
+        System.out.println("Список без дубликатов: " + uniqueNumbers);
+    }
+
+    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
+        Set<Integer> seen = new HashSet<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (Integer num : list) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+                result.add(num);
+            }
+        }
+        return result;
+    }
 }
