b63df6549ff66ebc849171aa31717a80cae066ed
Zhanyl <zh.kishkentai@gmail.com>
2025-02-26 10:04:33 +0500

    Выполнение задачи недкли 5 уровня medium

--- Module 5/Homework/src/five/medium/Main.java
+++ Module 5/Homework/src/five/medium/Main.java
@@ -1,12 +1,32 @@
 package five.medium;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
-  public static boolean isWeekend(DayOfWeek day) {
-    return false;
-  }
+        List<DayOfWeek> daysOfWeek = new ArrayList<>();
 
+
+        Collections.addAll(daysOfWeek, DayOfWeek.values());
+
+
+        System.out.println("Все дни недели:");
+        for (DayOfWeek day : daysOfWeek) {
+            System.out.println(day);
+        }
+
+
+        System.out.println("\nПроверка выходных:");
+        for (DayOfWeek day : daysOfWeek) {
+            System.out.println(day + " - выходной? " + isWeekend(day));
+        }
+    }
+
+    public static boolean isWeekend(DayOfWeek day) {
+        return day == DayOfWeek.суббота || day == DayOfWeek.воскресенье;
+    }
 }
